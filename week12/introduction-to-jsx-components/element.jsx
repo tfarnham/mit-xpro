7c2a8bce3c63a768e78f074480cb6d1d2dbd0c61
tfarnham <1356212+tfarnham@users.noreply.github.com>
2022-07-13 20:37:25 -0700

    Modify React Component

--- week12/introduction-to-jsx-components/element.jsx
+++ week12/introduction-to-jsx-components/element.jsx
@@ -1,4 +1,16 @@
-const name = 'Mary';
-const element = <h1> Hello {name} </h1>;
+// React Render a Component
 
-ReactDOM.render(element, document.getElementById('root'));
+const Hello = (props) => {
+    let color = props.action();
+    const name = props.name;
+    return <h1 style = {{backgroundColor:color}}> Hello {name} </h1>;
+};
+
+const getRandomColor = () => {
+    const palet = ["red","green","blue"];
+    let color = palet[Math.floor(Math.random()*3)];
+    console.log(color);
+    return color;
+}
+
+ReactDOM.render(<Hello action={getRandomColor} name="Francis"/>, document.getElementById('root'));
