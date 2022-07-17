b6e643644b7871a1e2a013a0a162565130683182
tfarnham <1356212+tfarnham@users.noreply.github.com>
2022-07-16 19:09:41 -0700

    Delete button onClick

--- week12/delete-button-exercise/getData0.jsx
+++ week12/delete-button-exercise/getData0.jsx
@@ -4,6 +4,11 @@ const App = () => {
     const handler = index => {
         // code to update state and remove destroyed button
         // [1,3,4]
+        let newState = state.filter((element, stateIndex) => {
+            console.log("Check stateIndex: ", stateIndex," index: ",index)
+            return !(index == stateIndex)
+        });
+        console.log("state: ",state);
         setState(newState)
     }
     let list = state.map((item, index) => {
@@ -14,7 +19,7 @@ const App = () => {
 
 const MyButton = ({ onClick }) => {
     let { Button } = ReactBootstrap
-    return <Button onClick={onClick}>Click Me</Button>
+    return <Button onClick={onClick}>Click Me5</Button>
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
