13bf77f602ec19530c0ee71c0f0e06a0fa0b1f28
tfarnham <1356212+tfarnham@users.noreply.github.com>
2022-09-06 19:51:58 -0700

    Expand context to more pages

--- week18/index/deposit.js
+++ week18/index/deposit.js
@@ -1,5 +1,10 @@
 function Deposit(){
+  const ctx = React.useContext(UserContext);
   return (
+    <>
     <h1>Deposit</h1>
+    <h5>All Data in Store
+    {JSON.stringify(ctx)}<br/></h5>
+    </>
   )
 }
