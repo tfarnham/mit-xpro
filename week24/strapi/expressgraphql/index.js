eb286eadc1a80948e63723a0418e357679717346
tfarnham <1356212+tfarnham@users.noreply.github.com>
2022-11-01 18:25:45 -0700

    GraphQL Intro
    Initial commit on GraphQL exercises

---
+++ week24/strapi/expressgraphql/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = { hello: () => 'Hello world!' };
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000, ()=> console.log("Running GraphQL on port 4000"));
