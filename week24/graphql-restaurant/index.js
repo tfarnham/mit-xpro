6a8b2ad2e89bdbc71f64bbc10b1ce42bfc74acd2
tfarnham <1356212+tfarnham@users.noreply.github.com>
2022-11-01 20:07:04 -0700

    Add delete functionality

--- week24/graphql-restaurant/index.js
+++ week24/graphql-restaurant/index.js
@@ -92,18 +92,28 @@ type Mutation{
 // The root provides a resolver function for each API endpoint
 
 var root = {
-  restaurant: (arg) => {
-    // Your code goes here
-  },
-  restaurants: () => {
-    // Your code goes here
-    restaurants : () => restaurants
-  },
+  restaurant: (arg) => restaurants[arg.id],
+
+  restaurants: () => restaurants,
+  
   setrestaurant: ({ input }) => {
-    // Your code goes here
+    restaurants.push( {name: input.name, description: input.description});
+    return input;
   },
   deleterestaurant: ({ id }) => {
-    // Your code goes here
+    let ok;
+    console.log("Response: " + ok);
+    let startlength = restaurants.length;
+    restaurants = restaurants.filter((item) => item.id !== id);
+    let finishlength = restaurants.length;
+    console.log("Start " + startlength + " End: " + finishlength);
+    if (startlength > finishlength) {
+      ok = true;
+    }
+    else {
+      ok = false;
+    }
+    return { ok }
   },
   editrestaurant: ({ id, ...restaurant }) => {
     // Your code goes here
@@ -118,7 +128,7 @@ app.use(
     graphiql: true,
   })
 );
-var port = 5500;
-app.listen(5500, () => console.log("Running Graphql on Port:" + port));
+var port = 5503;
+app.listen(5503, () => console.log("Running Graphql on Port:" + port));
 
 //export default root;
