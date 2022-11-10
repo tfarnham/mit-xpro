e0f5ec6a2e3a58a3d107f91c9828018d48fc28ec
tfarnham <1356212+tfarnham@users.noreply.github.com>
2022-11-09 20:03:34 -0800

    Start week 25

---
+++ week25/books/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app     = express();
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+app.get('/', (req, res) => {
+    res.send("Hello world!");
+})
+
+app.listen(3000,() => {
+    console.log("Running on port 3000");
+})
